0a0316b8c4aed67ffc0699e3f587eaa710153439
Robert Wozniak <robert.wozniak@snow.dog>
2019-02-26 15:37:25 +0100

    Add local testing file

---
+++ now.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const path = require('path');
+const mailer = require('./index.js');
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === path.normalize('/')) {
+    res.end('This connection is alive!');
+  } else if (req.url === path.normalize('/send')) {
+
+    try {
+      await mailer(req, res);
+
+      res.end('Sent with success')
+    } catch (err) {
+      res.statusCode = err.statusCode || 404
+      res.end(err.message)
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('this page doesn\'t exist');
+  }
+});
+
+server.listen(8080, '127.0.0.1');
