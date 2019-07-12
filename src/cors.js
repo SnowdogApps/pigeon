b0e9d980bf500124eefd6922645e179bc3861123
Bartek Igielski <igloczek@gmail.com>
2019-07-12 18:44:35 +0200

    CORS wildcard support

--- src/cors.js
+++ src/cors.js
@@ -1,6 +1,9 @@
 module.exports = (request, response, config, isDev = false) => {
   const origin = request.headers.origin
-  const allowedOrigins = config.cors.allowedOrigins
+  const allowedOrigins = config.allowedOrigins.map(url => {
+    const urlPattern = new RegExp(url.replace('*', '.+'))
+    return urlPattern.test(origin) ? origin : url
+  })
 
   if (isDev) {
     response.setHeader('Access-Control-Allow-Origin', '*')
