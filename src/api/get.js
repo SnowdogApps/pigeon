770fc6da46167c9711bda2699af3fed07511a3ce
Bartek Igielski <bartek.igielski@snow.dog>
2021-07-12 14:10:54 +0200

    Update URL parsing. Add request writability status checking. Add error logging.

--- src/api/get.js
+++ src/api/get.js
@@ -1,15 +1,18 @@
-const { parse } = require('url')
-
 const cors = require('../cors')
 const getConfig = require('../get-config')
 
 module.exports = (localConfig = {}, isDev = false) => async (request, response) => {
   try {
-    const params = parse(request.url, true).query
-    const config = await getConfig(params.id, localConfig, isDev)
+    const url = new URL(request.url, `http://${request.headers.host}`)
+    const id = url.searchParams.get('id')
+    const config = await getConfig(id, localConfig, isDev)
+
     cors(request, response, config, isDev)
-    response.end(JSON.stringify(config.fields))
+    if (!request.writableEnded) {
+      response.end(JSON.stringify(config.fields))
+    }
   } catch (err) {
+    console.error('Error while processing GET request.', err) // eslint-disable-line no-console
     response.statusCode = err.statusCode || 404
     response.end(err.message)
   }
--- src/api/post.js
+++ src/api/post.js
@@ -1,17 +1,19 @@
-const { parse } = require('url')
-
 const cors = require('../cors')
-const mailer = require('../mailer')
 const getConfig = require('../get-config')
+const mailer = require('../mailer')
 
 module.exports = (localConfig = {}, isDev = false) => async (request, response) => {
   try {
-    const params = parse(request.url, true).query
-    const config = await getConfig(params.id, localConfig, isDev)
+    const url = new URL(request.url, `http://${request.headers.host}`)
+    const id = url.searchParams.get('id')
+    const config = await getConfig(id, localConfig, isDev)
 
     cors(request, response, config, isDev)
-    await mailer(request, response, config, isDev)
+    if (!request.writableEnded) {
+      await mailer(request, response, config, isDev)
+    }
   } catch (err) {
+    console.error('Error while processing POST request.', err) // eslint-disable-line no-console
     response.statusCode = err.statusCode || 404
     response.end(err.message)
   }
